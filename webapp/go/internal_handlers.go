4674475672a3a0dcc5f03384662a613529bb6142
土屋健司 <tsuchiyakenji@M2-MacBookAir.local>
2024-12-08 15:36:27 +0900

    最も時給のいい椅子を割り当てる形に変更

--- webapp/go/internal_handlers.go
+++ webapp/go/internal_handlers.go
@@ -32,31 +32,33 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 		Chair
 		Latitude  int `db:"latitude"`
 		Longitude int `db:"longitude"`
+		Speed     int `db:"speed"`
 	}
 	chairsWithLocations := []ChairWithLocation{}
 	query := `
-        SELECT c.id, cl.latitude, cl.longitude
-        FROM chairs c
-        JOIN (
-            SELECT chair_id, latitude, longitude
-            FROM chair_locations
-            WHERE (chair_id, created_at) IN (
-                SELECT chair_id, MAX(created_at)
-                FROM chair_locations
-                GROUP BY chair_id
-            )
-        ) cl ON c.id = cl.chair_id
-        WHERE c.is_active = TRUE
-        AND c.id NOT IN (
-            SELECT r.chair_id
-            FROM rides r
-            JOIN ride_statuses rs ON r.id = rs.ride_id
-						WHERE chair_id IS NOT NULL
-						AND rs.chair_sent_at IS NOT NULL
-						AND rs.app_sent_at IS NOT NULL
-            GROUP BY r.chair_id, rs.ride_id
-            HAVING COUNT(rs.id) < 6
-        )
+			SELECT c.id, cl.latitude, cl.longitude, cm.speed
+			FROM chairs c
+			JOIN (
+					SELECT chair_id, latitude, longitude
+					FROM chair_locations
+					WHERE (chair_id, created_at) IN (
+							SELECT chair_id, MAX(created_at)
+							FROM chair_locations
+							GROUP BY chair_id
+					)
+			) cl ON c.id = cl.chair_id
+			JOIN chair_models cm ON c.model = cm.name
+			WHERE c.is_active = TRUE
+			AND c.id NOT IN (
+					SELECT r.chair_id
+					FROM rides r
+					JOIN ride_statuses rs ON r.id = rs.ride_id
+					WHERE chair_id IS NOT NULL
+					AND rs.chair_sent_at IS NOT NULL
+					AND rs.app_sent_at IS NOT NULL
+					GROUP BY r.chair_id, rs.ride_id
+					HAVING COUNT(rs.id) >= 6
+			)
 	`
 	if err := db.SelectContext(ctx, &chairsWithLocations, query); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
@@ -73,29 +75,33 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 
 	// 各リクエストに対して最も近い椅子を見つけて割り当てる
 	for _, ride := range rides {
-		var nearestChair *ChairWithLocation
-		minDistance := math.MaxInt
+		var bestChair *ChairWithLocation
+		minScore := math.MaxInt
 
 		for _, chair := range chairsWithLocations {
 			if assignedChairs[chair.ID] {
 				continue
 			}
 
-			distance := calculateDistance(ride.PickupLatitude, ride.PickupLongitude, chair.Latitude, chair.Longitude)
-			if distance < minDistance {
-				minDistance = distance
-				nearestChair = &chair
+			pickupDistance := calculateDistance(ride.PickupLatitude, ride.PickupLongitude, chair.Latitude, chair.Longitude)
+			destDistance := calculateDistance(ride.DestinationLatitude, ride.DestinationLongitude, ride.PickupLatitude, ride.PickupLongitude)
+			rideFee := 500 + destDistance*100
+			// 最も時給のいいものを選択
+			score := rideFee / ((pickupDistance + destDistance) / chair.Speed)
+			if score < minScore {
+				minScore = score
+				bestChair = &chair
 			}
 		}
 
-		if nearestChair != nil {
+		if bestChair != nil {
 			// ライドに椅子を割り当てる
-			if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", nearestChair.ID, ride.ID); err != nil {
+			if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", bestChair.ID, ride.ID); err != nil {
 				writeError(w, http.StatusInternalServerError, err)
 				return
 			}
 			// 割り当て済みの椅子としてマーク
-			assignedChairs[nearestChair.ID] = true
+			assignedChairs[bestChair.ID] = true
 		}
 	}
 
