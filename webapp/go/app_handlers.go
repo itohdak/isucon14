c8fc75d2255b5957c27c6b72cbf1dd15108f7d40
itohdak <itohdak@gmail.com>
2024-12-28 17:30:04 +0900

    resolve N+1 in getChairStats

--- webapp/go/app_handlers.go
+++ webapp/go/app_handlers.go
@@ -772,53 +772,22 @@ func appGetNotification(w http.ResponseWriter, r *http.Request) {
 func getChairStats(ctx context.Context, tx *sqlx.Tx, chairID string) (appGetNotificationResponseChairStats, error) {
 	stats := appGetNotificationResponseChairStats{}
 
-	rides := []Ride{}
-	err := tx.SelectContext(
-		ctx,
-		&rides,
-		`SELECT * FROM rides WHERE chair_id = ? ORDER BY updated_at DESC`,
-		chairID,
-	)
-	if err != nil {
-		return stats, err
-	}
-
 	totalRideCount := 0
 	totalEvaluation := 0.0
-	for _, ride := range rides {
-		rideStatuses := []RideStatus{}
-		err = tx.SelectContext(
-			ctx,
-			&rideStatuses,
-			`SELECT * FROM ride_statuses WHERE ride_id = ? ORDER BY created_at`,
-			ride.ID,
-		)
-		if err != nil {
-			return stats, err
-		}
-
-		var arrivedAt, pickupedAt *time.Time
-		var isCompleted bool
-		for _, status := range rideStatuses {
-			if status.Status == "ARRIVED" {
-				arrivedAt = &status.CreatedAt
-			} else if status.Status == "CARRYING" {
-				pickupedAt = &status.CreatedAt
-			}
-			if status.Status == "COMPLETED" {
-				isCompleted = true
-			}
-		}
-		if arrivedAt == nil || pickupedAt == nil {
-			continue
-		}
-		if !isCompleted {
-			continue
-		}
-
-		totalRideCount++
-		totalEvaluation += float64(*ride.Evaluation)
+	var ret = struct {
+		TotalRideCount  int64 `db:"total_ride_count"`
+		TotalEvaluation int64 `db:"total_evaluation"`
+	}{}
+	if err := tx.GetContext(
+		ctx,
+		&ret,
+		`SELECT IFNULL(COUNT(1), 0) AS total_ride_count, IFNULL(SUM(r1.evaluation), 0) AS total_evaluation FROM rides r1, ride_statuses r2 WHERE r1.id = r2.ride_id AND r2.status = 'COMPLETED' AND r1.chair_id = ?`,
+		chairID,
+	); err != nil {
+		return stats, err
 	}
+	totalRideCount = int(ret.TotalRideCount)
+	totalEvaluation = float64(ret.TotalEvaluation)
 
 	stats.TotalRidesCount = totalRideCount
 	if totalRideCount > 0 {
