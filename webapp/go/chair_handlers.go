602e718d590dd47172415d45afb28ec0227f2d97
itohdak <itohdak@gmail.com>
2025-01-12 20:36:49 +0900

    debug

--- webapp/go/chair_handlers.go
+++ webapp/go/chair_handlers.go
@@ -117,6 +117,7 @@ func chairPostCoordinate(w http.ResponseWriter, r *http.Request) {
 	defer tx.Rollback()
 
 	createdAt := time.Now()
+	time.Sleep(50 * time.Millisecond)
 	updateCoordinateQueue <- CoordinateToUpdate{
 		ChairLocationID: ulid.Make().String(),
 		ChairID:         chair.ID,
