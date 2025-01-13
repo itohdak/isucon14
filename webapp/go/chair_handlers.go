296496aeb697450218c7860948559f906d2aa920
itohdak <itohdak@gmail.com>
2025-01-13 14:05:28 +0900

    decrease timeout

--- webapp/go/chair_handlers.go
+++ webapp/go/chair_handlers.go
@@ -199,7 +199,7 @@ func chairPostCoordinate(w http.ResponseWriter, r *http.Request) {
 func updateCoordinates() {
 	var coordinates []CoordinateToUpdate
 	var maxLength = 2000
-	var timeout = 300 * time.Millisecond
+	var timeout = 30 * time.Millisecond
 	now := time.Now()
 	for {
 		select {
