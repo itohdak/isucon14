b8e89c10a364dc0da2b1cd63de4cebf903251ccc
itohdak <itohdak@gmail.com>
2025-01-14 00:46:40 +0900

    add index to avoid locks

--- webapp/sql/99-patch.sql
+++ webapp/sql/99-patch.sql
@@ -1,2 +1,3 @@
 ALTER TABLE rides ADD sales INTEGER DEFAULT 0;
 ALTER TABLE chairs ADD is_available TINYINT(1) INVISIBLE NOT NULL DEFAULT 1;
+CREATE INDEX idx_is_active_is_available_id ON chairs (is_active, is_available, id);
