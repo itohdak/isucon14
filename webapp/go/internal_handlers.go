b0203ab68b9961e2799c0e9474a8e69e1c4e5a8f
itohdak <itohdak@gmail.com>
2025-01-13 11:23:30 +0900

    refine query

--- webapp/go/internal_handlers.go
+++ webapp/go/internal_handlers.go
@@ -1,20 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"time"
-
-	"github.com/jmoiron/sqlx"
 )
+
+type ChairWithLatLon struct {
+	ID          string    `db:"id"`
+	OwnerID     string    `db:"owner_id"`
+	Name        string    `db:"name"`
+	Model       string    `db:"model"`
+	IsActive    bool      `db:"is_active"`
+	AccessToken string    `db:"access_token"`
+	CreatedAt   time.Time `db:"created_at"`
+	UpdatedAt   time.Time `db:"updated_at"`
+
+	Latitude  int `db:"latitude"`
+	Longitude int `db:"longitude"`
+}
 
 // このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
 func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	// MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
 	rides := []Ride{}
-	if err := db.SelectContext(ctx, &rides, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at FOR UPDATE SKIP LOCKED`); err != nil {
+	if err := db.SelectContext(ctx, &rides, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at`); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
 		return
 	}
@@ -23,58 +34,60 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	chairs := []Chair{}
-	if err := db.SelectContext(
-		ctx,
-		&chairs,
-		`SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE (SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = chairs.id) GROUP BY ride_id) is_completed WHERE completed = FALSE) AND is_active = TRUE ORDER BY RAND()) AS tmp ON chairs.id = tmp.id FOR UPDATE SKIP LOCKED`); err != nil {
+	chairs := []ChairWithLatLon{}
+	if err := db.Select(&chairs, `
+ WITH chair_latest_location AS (
+ 	SELECT *
+ 	FROM (
+ 		SELECT chair_locations.*, ROW_NUMBER() OVER (PARTITION BY chair_id ORDER BY created_at DESC) AS rn
+ 		FROM chair_locations
+ 	) c
+ 	WHERE c.rn = 1
+ ),
+ chair_latest_status AS (
+ 	SELECT *
+ 	FROM (
+ 		SELECT rides.*, ride_statuses.status AS ride_status, ROW_NUMBER() OVER (PARTITION BY chair_id ORDER BY ride_statuses.created_at DESC) AS rn
+ 		FROM rides INNER JOIN ride_statuses ON rides.id = ride_statuses.ride_id AND ride_statuses.chair_sent_at IS NOT NULL -- この条件は椅子の通知エンドポイントの実装で、未送信の状態がある2つ以上の異なるライドが割り当てられていても正しく順番に送るように修正していれば不要
+ 	) r
+ 	WHERE r.rn = 1
+ )
+ SELECT
+ 	chairs.*, chair_latest_location.latitude, chair_latest_location.longitude
+ FROM chairs
+ LEFT JOIN chair_latest_status ON chairs.id = chair_latest_status.chair_id
+ LEFT JOIN chair_latest_location ON chairs.id = chair_latest_location.chair_id
+ WHERE
+ 	(chair_latest_status.ride_status = 'COMPLETED' OR chair_latest_status.ride_status IS NULL) AND chairs.is_active`); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
+		return
 	}
 	if len(chairs) == 0 {
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
-	chairIDs := make([]string, len(chairs))
-	chairMap := map[string]Chair{}
-	for i, chair := range chairs {
-		chairIDs[i] = chair.ID
-		chairMap[chair.ID] = chair
-	}
 
-	locations := []ChairLocation{}
-	query := `SELECT l1.* FROM chair_locations l1 JOIN (SELECT chair_id, MAX(created_at) AS created_at FROM chair_locations l2 GROUP BY chair_id) AS tmp ON l1.chair_id = tmp.chair_id AND l1.created_at = tmp.created_at WHERE l1.chair_id IN (?)`
-	query, param, err := sqlx.In(query, chairIDs)
-	if err != nil {
-		writeError(w, http.StatusInternalServerError, fmt.Errorf("failed to prepare in query: %v", err))
-		return
-	}
-	err = db.SelectContext(ctx, &locations, query, param...)
-	if err != nil {
-		writeError(w, http.StatusInternalServerError, fmt.Errorf("failed to exec in query: %v", err))
-		return
-	}
-
-	n, m := len(rides), len(locations)
+	n, m := len(rides), len(chairs)
 	g := newMinCostFlow(n + m + 2)
 	s, t := n+m, n+m+1
 	for i, _ := range rides {
 		g.AddEdge(s, i, 1, 0)
 	}
-	for i, _ := range locations {
+	for i, _ := range chairs {
 		g.AddEdge(n+i, t, 1, 0)
 	}
 	for i, ride := range rides {
-		for j, location := range locations {
+		for j, chair := range chairs {
 			var model = ChairModel{
 				Speed: 1,
 			}
-			if modelCached, found := chairModelCache.Load(chairMap[location.ChairID].Model); found {
+			if modelCached, found := chairModelCache.Load(chair.Model); found {
 				model = modelCached.(ChairModel)
 			} else {
-				log.Printf("chair model not found: model name: %s", chairMap[location.ChairID].Model)
+				log.Printf("chair model not found: model name: %s", chair.Model)
 			}
-			cost := max((abs(ride.PickupLatitude-location.Latitude)+
-				abs(ride.PickupLongitude-location.Longitude)+
+			cost := max((abs(ride.PickupLatitude-chair.Latitude)+
+				abs(ride.PickupLongitude-chair.Longitude)+
 				abs(ride.DestinationLatitude-ride.PickupLatitude)+
 				abs(ride.DestinationLongitude-ride.PickupLongitude))/model.Speed-int(time.Now().Sub(ride.CreatedAt).Seconds())*10, 0)
 			g.AddEdge(i, n+j, 1, cost)
@@ -88,7 +101,7 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 		}
 		matchedRideID := rides[e.from].ID
 		matchedUserID := rides[e.from].UserID
-		matchedChairID := locations[e.to-n].ChairID
+		matchedChairID := chairs[e.to-n].ID
 		log.Printf("matched ride %s with chair %s\n", matchedChairID, matchedRideID)
 		db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matchedChairID, matchedRideID)
 		userRideCache.Delete(matchedUserID)
