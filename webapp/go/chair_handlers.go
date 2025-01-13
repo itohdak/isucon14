7fdaa14b77ebf865a7cff108afaa81505f14a900
itohdak <itohdak@gmail.com>
2025-01-13 15:15:27 +0900

    tune postCoordinate delay

--- webapp/go/chair_handlers.go
+++ webapp/go/chair_handlers.go
@@ -118,7 +118,7 @@ func chairPostCoordinate(w http.ResponseWriter, r *http.Request) {
 	defer tx.Rollback()
 
 	createdAt := time.Now()
-	time.Sleep(20 * time.Millisecond)
+	time.Sleep(30 * time.Millisecond)
 	updateCoordinateQueue <- CoordinateToUpdate{
 		ChairLocationID: ulid.Make().String(),
 		ChairID:         chair.ID,
